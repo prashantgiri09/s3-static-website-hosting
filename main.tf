a57f2de27a4e8ba4a16834b4984990e71b80f278
prashantgiri09 <connecttoprashantgiri@gmail.com>
2026-04-02 10:29:01 +0530

    Add files via upload
    Added css, js and images

---
+++ main.tf
@@ -0,0 +1,1 @@
+//write a program in cpp of palindrome numberswhat ii sdns
---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const typed = new Typed(".auto-type", {
+      strings: [
+        "<span style='color:#b74b4b;'>Cloud Enthusiast</span>",
+        "<span style='color:#4b9cb7;'>Aspiring DevOps Engineer</span>",
+        "<span style='color:#4bb76a;'>IaC Learner</span>",
+        "<span style='color:#b79f4b;'>AWS & Terraform Explorer</span>",
+        "<span style='color:#9b4bb7;'>Software Developer</span>"
+      ],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
+  });
+  
